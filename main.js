709267afebfda390027bf62c7e05f6e97e22f45b
HasanliRasim <https://github.com/HasanliRasim>
2024-04-23 00:02:08 +0300

    ADY

---
+++ main.js
@@ -0,0 +1,79 @@
+const element = document.querySelector('#main');
+element.innerHTML = `<table style="border: 1px solid #000;">
+        <tr>
+            <td style="border-bottom: 1px solid #000;">Qatarın statusu və nömrəsi</td>
+            <td style="border-bottom: 1px solid #000;">Bakı</td>
+            <td style="border-bottom: 1px solid #000;">Biləcəri</td>
+            <td style="border-bottom: 1px solid #000;">Xırdalan</td>
+            <td style="border-bottom: 1px solid #000;">Sumqayıt</td>
+        </tr>
+        <tr>
+            <td style="border-bottom: 1px solid #000;">№ 6001</td>
+            <td style="border-bottom: 1px solid #000;">07:20</td>
+            <td style="border-bottom: 1px solid #000;">07:34</td>
+            <td style="border-bottom: 1px solid #000;">07:42</td>
+            <td style="border-bottom: 1px solid #000;">08:00</td>
+        </tr>
+        <tr>
+            <td style="border-bottom: 1px solid #000;">№ 6002</td>
+            <td style="border-bottom: 1px solid #000;">07:45</td>
+            <td style="border-bottom: 1px solid #000;">07:59</td>
+            <td style="border-bottom: 1px solid #000;">08:06</td>
+            <td style="border-bottom: 1px solid #000;">-</td>
+        </tr>
+        <tr>
+            <td style="border-bottom: 1px solid #000;">№ 6003</td>
+            <td style="border-bottom: 1px solid #000;">07:20</td>
+            <td style="border-bottom: 1px solid #000;">07:34</td>
+            <td style="border-bottom: 1px solid #000;">07:42</td>
+            <td style="border-bottom: 1px solid #000;">08:00</td>
+        </tr>
+        <tr>
+            <td style="border-bottom: 1px solid #000;">№ 6004</td>
+            <td style="border-bottom: 1px solid #000;">08:30</td>
+            <td style="border-bottom: 1px solid #000;">08:44</td>
+            <td style="border-bottom: 1px solid #000;">08:52</td>
+            <td style="border-bottom: 1px solid #000;">09:10</td>
+        </tr>
+        <tr>
+            <td style="border-bottom: 1px solid #000;">№ 6005</td>
+            <td style="border-bottom: 1px solid #000;">17:00</td>
+            <td style="border-bottom: 1px solid #000;">17:14</td>
+            <td style="border-bottom: 1px solid #000;">17:22</td>
+            <td style="border-bottom: 1px solid #000;">17:40</td>
+        </tr>
+        <tr>
+            <td style="border-bottom: 1px solid #000;">№ 6006</td>
+            <td style="border-bottom: 1px solid #000;">07:20</td>
+            <td style="border-bottom: 1px solid #000;">07:34</td>
+            <td style="border-bottom: 1px solid #000;">07:42</td>
+            <td style="border-bottom: 1px solid #000;">08:00</td>
+        </tr>
+        <tr>
+            <td style="border-bottom: 1px solid #000;">№ 6007</td>
+            <td style="border-bottom: 1px solid #000;">18:05</td>
+            <td style="border-bottom: 1px solid #000;">-</td>
+            <td style="border-bottom: 1px solid #000;">-</td>
+            <td style="border-bottom: 1px solid #000;">18:45</td>
+        </tr>
+        <tr>
+            <td style="border-bottom: 1px solid #000;">№ 6008</td>
+            <td style="border-bottom: 1px solid #000;">18:20</td>
+            <td style="border-bottom: 1px solid #000;">18:34</td>
+            <td style="border-bottom: 1px solid #000;">18:42</td>
+            <td style="border-bottom: 1px solid #000;">19:00</td>
+        </tr>
+        <tr>
+            <td style="border-bottom: 1px solid #000;">№ 6009</td>
+            <td style="border-bottom: 1px solid #000;">07:20</td>
+            <td style="border-bottom: 1px solid #000;">07:34</td>
+            <td style="border-bottom: 1px solid #000;">07:42</td>
+            <td style="border-bottom: 1px solid #000;">08:00</td>
+        </tr>
+        <tr>
+            <td>№ 6010</td>
+            <td>18:50</td>
+            <td>19:04</td>
+            <td>19:12</td>
+            <td>19:30</td>
+        </tr></table>`;
